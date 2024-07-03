6bb6735d8a82d3092f1f7116365252dd2ba31479
lhj6102 <lhj6102@gmail.com>
2024-07-03 15:50:06 +0900

    first commit

---
+++ tests/dataSets.ts
@@ -0,0 +1,39 @@
+const data = [
+  {
+    characterName: "푸딩몰래먹은사람",
+    className: "소서리스",
+    id: 4,
+    sortable: {
+      skillDPS: {
+        "종말의 날": 15000000,
+        천벌: 10000000,
+        익스플로전: 11000000,
+      },
+      skillDamage: {
+        "종말의 날": 11000000,
+        "종말의 날 - 마력 해방": 19000000,
+        천벌: 9000000,
+        익스플로전: 21000000,
+        버스트: 300000000,
+      },
+      buffRatio: {},
+    },
+    filterable: {
+      engravings: ["원한", "저주받은 인형", "버스트"],
+      selectedSkills: ["종말의 날", "천벌", "익스플로전", "애로우 해일"],
+      tierSets: ["2악4지"],
+      stats: ["특치"],
+    },
+  },
+  {
+    characterName: "주다영",
+    className: "바드",
+    id: 5,
+    sortable: {
+      skillDPS: {},
+      skillDamage: {},
+    },
+  },
+];
+
+// output
