c4a2dd5bc9be748af0567dc52bab5bd4f22322f8
lhj6102 <lhj6102@gmail.com>
2024-07-07 20:34:28 +0900

    fix: Lower test size

--- ___test___/genData/generateRandomSorcDataSet.test.ts
+++ ___test___/genData/generateRandomSorcDataSet.test.ts
@@ -2,6 +2,6 @@ import generateRandomSorcDataSet from "./generateRandomSorcDataSet";
 
 describe("create gen data", () => {
   test("should create gen data", () => {
-    generateRandomSorcDataSet(100000);
+    generateRandomSorcDataSet(50000);
   });
 });
