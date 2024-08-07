e1fc6b1a30b148045feff049231584f0d17f4963
lhj6102 <lhj6102@gmail.com>
2024-08-07 10:58:40 +0900

    feat: Remove declaration

--- dist/cjs/encode/generateKeys.d.ts
+++
@@ -1,6 +0,0 @@
-import { Key } from "../models/Keys";
-import RawData from "../models/RawData";
-export declare function generateKeys(data: RawData<any>): {
-    sortKey: Key<string[]>;
-    filterKey: Key<string[]>;
-};
